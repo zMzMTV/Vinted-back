979f52f666cbde4821ee104025cba5e78feedd29
zMzMTV <yassineazem@yahoo.fr>
2020-11-16 14:03:31 +0100

    start script

--- index.js
+++ index.js
@@ -31,6 +31,6 @@ app.all("*", (req, res) => {
   res.status(404).json({ message: "Cette route n'existe pas" });
 });
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 3001, () => {
   console.log("Server Started");
 });
