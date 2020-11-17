92647d8b508e27e896f990e1b59ded134f9a1b21
zMzMTV <yassineazem@yahoo.fr>
2020-11-17 16:43:50 +0100

    try to solve pb

--- index.js
+++ index.js
@@ -32,6 +32,6 @@ app.all("*", (req, res) => {
   res.status(404).json({ message: "Cette route n'existe pas" });
 });
 
-app.listen(process.env.PORT || 3001, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server Started");
 });
